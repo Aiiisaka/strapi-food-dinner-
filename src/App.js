27a87c58c332f213d8c4a6be1b7a499929822952
V_Huard <21902424@etu.unicaen.fr>
2021-10-15 10:48:39 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default class App extends React.Component {
   state = {
     allPlats: [],
     allMenus: [],
-    error: null,
+    error: null, 
   };
 
   componentDidMount = async () => {
