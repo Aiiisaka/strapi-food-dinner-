93c8f1006a983f86d7dc898677ef89e77d1b0bd8
AsthinoZafiamy <azafiamy@infodev.ovh>
2021-10-16 23:58:16 +0000

    [FONCTIONNALITE] : Suppression de plat interactif

--- src/components/Liste.js
+++ src/components/Liste.js
@@ -1,78 +1,93 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import ListePlats from './ListePlat.js';
 import ListeMenus from './ListeMenu.js';
 
-import { Header, List, Grid, Image } from 'semantic-ui-react'
+import { Header } from 'semantic-ui-react'
 
-const Liste = ({ plats, menus, match }) => {
-    let filtrage; let isMenu = false;
+const Liste = ({plats, menus, match }) => {
+    const [data, setData] = useState([]);
+    const [filtrage, setFiltrage] = useState([]);
+    const [isMenu, setIsMenu] = useState(false);
 
-    switch (match.params.filter) {
-        case "entrees":
-            isMenu = false;
-            filtrage = plats.filter(function (plat) {
-                return plat.category.nom === "Entrée";
-            });
-            break;
-        case "plats":
-            isMenu = false;
-            filtrage = plats.filter(function (plat) {
-                return plat.category.nom === "Plat";
-            });
-            break;
-        case "desserts":
-            isMenu = false;
-            filtrage = plats.filter(function (plat) {
-                return plat.category.nom === "Dessert";
-            });
-            break;
-        case "boissons":
-            isMenu = false;
-            filtrage = plats.filter(function (plat) {
-                return plat.category.nom === "Boisson";
-            });
-            break;
-        default:
-            isMenu = true;
-            filtrage = menus;
-    }
+    useEffect(() => {
+        setData(plats);
+    },[plats]);
+
+    useEffect(() => {
+        switch (match.params.filter) {
+            case "entrees":
+                setIsMenu(false);
+                setFiltrage(data.filter(function (plat) {
+                    return plat.category.nom === "Entrée";
+                }));
+                break;
+            case "plats":
+                setIsMenu(false);
+                setFiltrage(data.filter(function (plat) {
+                    return plat.category.nom === "Plat";
+                }));
+                break;
+            case "desserts":
+                setIsMenu(false);
+                setFiltrage(data.filter(function (plat) {
+                    return plat.category.nom === "Dessert";
+                }));
+                break;
+            case "boissons":
+                setIsMenu(false);
+                setFiltrage(data.filter(function (plat) {
+                    return plat.category.nom === "Boisson";
+                }));
+                break;
+            default:
+                setIsMenu(true);
+                setFiltrage(menus);
+        }
+    },[data, match]);
 
     if (isMenu) {
-        return (
-            <>
-                <div class="banner">
-                    <div class="border"> </div>
-                    <h2>Straπ Food</h2>
-                    <p>by Valentin, Maud, Asthino, Hadrien</p>
-                    <div class="border border-bottom">
+            return (
+                <>
+                    <div className="banner">
+                        <div className="border"> </div>
+                        <h2>Straπ Food</h2>
+                        <p>by Valentin, Maud, Asthino, Hadrien</p>
+                        <div className="border border-bottom">
+                        </div>
                     </div>
-                </div>
-                <div class="menu-list">
-                    <div class="demo-description">
-                        <h1 class="demo-description__title">StraPi Food Dinner</h1>
-                        <p class="demo-description__p">Cliquez sur les noms des menus pour voir les compositions</p>
+                    <div className="menu-list">
+                        <div className="demo-description">
+                            <h1 className="demo-description__title">StraPi Food Dinner</h1>
+                            <p className="demo-description__p">Cliquez sur les noms des menus pour voir les compositions</p>
+                        </div>
+                        <div className="demo coursemeal">
+                            {
+                                filtrage.map((menu) => < ListeMenus menu={menu} key={menu.id} />)
+                            }
+                        </div>
                     </div>
-                    <div class="demo coursemeal">
+                </>
+            )
+        } else {
+            return (
+                <>
+                    <Header as='h1' color='brown' textAlign="center">Liste des Plats</Header>
+                    <div className="masonry">
                         {
-                            filtrage.map((menu) => < ListeMenus menu={menu} key={menu.id} />)
+                            filtrage.map((plat) => < ListePlats 
+                                onDeletePlat = {
+                                    (idPlat) => setData(prevData => {
+                                        prevData.splice(prevData.findIndex(element => element.id === idPlat),1); 
+                                        return [...prevData]
+                                    })
+                                } 
+                            plat={plat} key={plat.id} />)
                         }
                     </div>
-                </div>
-            </>
-        )
-    } else {
-        return (
-            <>
-                <Header as='h1' color='brown' textAlign="center">Liste des Plats</Header>
-                <div class="masonry">
-                    {
-                        filtrage.map((plat) => < ListePlats plat={plat} key={plat.id} />)
-                    }
-                </div>
-            </>
-        )
-    }
+                </>
+            )
+        }
 }
 
 export default Liste
--- src/components/ListePlat.js
+++ src/components/ListePlat.js
@@ -1,18 +1,54 @@
 import React from 'react';
 
+// Parses the JSON returned by a network request
+//const parseJSON = resp => (resp.json ? resp.json() : resp);
+
+// // Checks if a network request came back fine, and throws an error if not
+// const checkStatus = resp => {
+//     if (resp.status >= 200 && resp.status < 300) {
+//         return resp;
+//     }
+//     return parseJSON(resp).then(resp => {
+//         throw resp;
+//     });
+// };
+
+const headers = {
+    'Content-Type': 'application/json',
+};
+
 export default class ListePlat extends React.Component {
+
+    deletePlat = (idPlat) => {
+        try {
+            fetch('http://54.37.165.18:1337/plats/'+ idPlat , {
+                method: 'DELETE',
+                headers: headers,
+            });
+            this.props.onDeletePlat(idPlat);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     render() {
         return (
             <>
-                <div class="grid">
-                    <img src={this.props.plat.image} />
-                    <div class="grid__body">
-                        <div class="relative">
-                            <h1 class="grid__title">{this.props.plat.nom}</h1>
-                            <p class="grid__author">{this.props.plat.description}</p>
+                <div className="grid">
+                    <div className="mt-0">
+                        <div className="grid__control_icons">
+                            <i onClick={() => {}} className="grid__icon modify inverted pencil alternate icon"></i>
+                            <i onClick={() => this.deletePlat(this.props.plat.id)} className="grid__icon kill inverted trash alternate icon"></i>
                         </div>
-                        <div class="mt-auto" >
-                            <span class="grid__tag">{this.props.plat.prix} euros</span>
+                    </div>
+                    <img src={this.props.plat.image} />
+                    <div className="grid__body">
+                        <div className="relative">
+                            <h1 className="grid__title">{this.props.plat.nom}</h1>
+                            <p className="grid__author">{this.props.plat.description}</p>
+                        </div>
+                        <div className="mt-auto" >
+                            <span className="grid__tag">{this.props.plat.prix} euros</span>
                         </div>
                     </div>
                 </div>
